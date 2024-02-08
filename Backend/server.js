384405a8b4d0525d9995f8a25673e3d2be22faa4
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2024-02-08 17:53:07 +0530

    MongoDB connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const {connect} = require("./db")
 const app = express();
+const port = process.env.PORT
+
+const connectionStatus = connect()
+connectionStatus.then(result=>{
+  app.route("/").get((req, res) => {
+    res.send(result?"Connected to DB":"Disconnected");
+  });
+  
+});
 
 app.route("/ping").get((req, res) => {
   res.send("Hello world, This is ping Route");
 });
-app.route("/").get((req, res) => {
-  res.send("Hello world, This is main Route(/)");
-});
 
-app.listen(3000, () => {
-  console.log(`App is running on PORT: 3000`);
+app.listen(port, () => {
+  console.log(`App is running on PORT: ${port}`);
 });
