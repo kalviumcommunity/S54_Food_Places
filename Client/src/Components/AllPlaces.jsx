d665fbcce769c0ff2b607e3e6dce03ed66c644a8
unknown <sp577152@gmail.com>
2024-02-25 22:02:30 +0530

    All Entities

--- Client/src/Components/AllPlaces.jsx
+++ Client/src/Components/AllPlaces.jsx
@@ -1,25 +1,41 @@
-import { Box, Heading } from "@chakra-ui/react";
-import axios from 'axios'
+import { Box, Button, Flex, Heading, Spinner } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
+import Entity from "./Entity";
 
-const AllPlaces = () => {
-  const [data, setData] = useState([]);
-  const API_URI = import.meta.env.VITE_API_URI
-  const fetchData = async()=>{
-    try {
-        const res = await axios.get(API_URI)
-        console.log(res);
-        
-    } catch (error) {
-        
-    }
-  }
-  useEffect(() => {
-    fetchData()
-  }, []);
+const AllPlaces = ({data,fetched}) => {
+  const [show, setShow] = useState(false);
   return (
     <Box>
       <Heading textAlign={"center"}>Explore Food Places</Heading>
+      <Flex wrap={"wrap"} my={"3vw"}>
+        {fetched ? (
+          show ? (
+            data.map((e, i) => {
+              return <Entity key={i} data={e} length={data.length}/>;
+            })
+          ) : (
+            data.slice(0, 9).map((e, i) => {
+              return <Entity key={i} data={e} length={data.length}/>;
+            })
+          )
+        ) : (
+          <Spinner
+            margin={"auto"}
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="#ad8f76"
+            size="xl"
+          />
+        )}
+      </Flex>
+      {fetched && (
+        <Flex justify={'center'} my={'2vw'}>
+            <Button onClick={() => setShow(!show)} >
+              {show ? "Show Less" : "Show More"}
+            </Button>
+        </Flex>
+      )}
     </Box>
   );
 };
--- Client/src/Components/Entity.jsx
+++ Client/src/Components/Entity.jsx
@@ -1,54 +1,50 @@
 import { StarIcon } from "@chakra-ui/icons";
-import { Badge, Box, Image, Flex, Heading } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Image,
+} from "@chakra-ui/react";
 import React from "react";
 
-const Entity = () => {
-  const data = {
-    _id: "65cc4c476fb792c7ef3bdc3c",
-    ListingId: 1,
-    Name: "Kitchen At 95",
-    Image:
-      "https://media-cdn.tripadvisor.com/media/photo-o/0e/5b/5c/b3/kitchen-at-95-multi-cuisine.jpg",
-    Rating: 5,
-    Location: "Ferozepur Road, Ludhiana 141012 India",
-    SpendPerPerson: "₹800 - ₹2,000",
-    Cuisines: "Chinese, Indian, Asian, Italian, International",
-    OpenHours: "06:30 AM - 12:00 AM",
-    PhoneNumber: "+91 82840 00231",
-    Website: "http://ludhiana.regency.hyatt.com/en/hotel/our-hotel.html",
-    Email: "atul.bhasin@hyatt.com",
-    PostedBy: "spjyotiranjan",
-  };
+const Entity = ({ data, length }) => {
   return (
-    <div>
+    <>
       <Box
         maxW="sm"
+        minW={"sm"}
         borderWidth="1px"
         borderRadius="lg"
         overflow="hidden"
         my={"2vw"}
         mx={"auto"}
         boxShadow={"0 0 0.5vw #00000026"}
+        _hover={{
+          transform: "scale(1.03)",
+          transition: "transform 0.3s",
+        }}
+        cursor={"pointer"}
       >
-        <Image src={data.Image} />
+        <Image src={data.Image} w={"100%"} h={"40vh"} />
 
         <Box p="6">
           <Box display="flex" alignItems="baseline">
-            <Badge
-              borderRadius="md"
-              px="2"
-              bgColor={"#ddc3ac"}
-              color="#00000099"
-            >
-              New
-            </Badge>
+            {data.ListingId > length - 5 && (
+              <Badge
+                borderRadius="md"
+                px="2"
+                bgColor={"#ddc3ac"}
+                color="#00000099"
+              >
+                New
+              </Badge>
+            )}
             <Box
               color="gray.500"
               fontWeight="semibold"
               letterSpacing="wide"
               fontSize="xs"
               textTransform="uppercase"
-              ml="2"
+              ml={data.ListingId > length - 5 && "2"}
             >
               {data.OpenHours.includes(",")
                 ? "Changes Day to Day"
@@ -85,7 +81,7 @@ const Entity = () => {
           </Box>
         </Box>
       </Box>
-    </div>
+    </>
   );
 };
 
